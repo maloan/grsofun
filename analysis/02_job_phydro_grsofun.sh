5a3626c2c7b78273bfb552462084e9d0e67a45cc
maloan <loumaran@outlook.com>
2026-03-30 10:37:32 +0200

    reformat and renaming variables

--- analysis/02_job_phydro_grsofun.sh
+++ analysis/02_job_phydro_grsofun.sh
@@ -2,23 +2,18 @@
 #SBATCH --job-name=PM-S0_grsofun
 #SBATCH --output=log/%x_%j.log
 #SBATCH --error=log/%x_%j.err
-#SBATCH --time=02:00:00
+#SBATCH --time=03:00:00
 #SBATCH --mail-type=end,fail
 #SBATCH --partition=icpu-stocker
 #SBATCH --account=invest
 #SBATCH --qos=job_icpu-stocker
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=32
 
 set -euo pipefail
 
 module purge
 module load R/4.4.2-gfbf-2024a UDUNITS/2.2.28-GCCcore-13.3.0 \
 PROJ/9.4.1-GCCcore-13.3.0 GDAL/3.10.0-foss-2024a CDO/2.4.4-gompi-2024a
-
-## To change
-PREFIX=PM-S0
-START=2000
-END=2018
 
 # export GRSOFUN_DIR="$HOME/grsofun" # grsofun repository needs to be cloned
 export R_LIBS_USER=$HOME/R/x86_64-pc-linux-gnu-library/4.4
@@ -55,5 +50,5 @@ remotes::install_github(
 
 # --- Run the script -----------------------------------------------------------
 # Rscript "$GRSOFUN_DIR/analysis/run_grsofun.R" # For remote
-Rscript run_grsofun.R # For within grsofun
+Rscript run_grsofun_modis.R # For within grsofun
 
